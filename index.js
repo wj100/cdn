9f47e6287f1c30e1dd4f7ff3cda418b6237263a6
wangjun <wb.wangjun21@mesg.corp.netease.com>
2023-01-11 18:34:35 +0800

    readme

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const github = require('octonode');
 // 生成的github Token
-const client = github.client('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');
+const client = github.client('');
 // 仓库名称，比如file-cdn
 const ghrepo = client.repo('wj100/cdn');
 const express = require('express')
@@ -20,6 +20,7 @@ app.post('/upload', (req, res) => {
   const fileType = fileArr[fileArr.length -1]
   const md5Name = `${uuidv1()}.${fileType}`
   ghrepo.createContents(md5Name, 'Upload new file', file.data, function (err, status, body, headers) {
+    console.log(err)
     res.send({
       // 上传后的文件url
       url: `https://raw.githubusercontent.com/wj100/cdn/master/${md5Name}`
